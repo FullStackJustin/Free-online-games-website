d6e857f6a154bf233c01f946a4a2f4511b6f2eaf
FullStackJustin <ju.tay.lor2019@gmail.com>
2022-08-16 23:39:31 +0000

    syntax fixes

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes} from 'react-router-dom'}
-import {Navbar} from '../components/navbar'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {Navbar} from './components/navbar'
 
 
 export const Layout = () => {
